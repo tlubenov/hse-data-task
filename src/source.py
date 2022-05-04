485dfa3666368d209658aa26ed701837a7d7ecac
Todor <tlubenov@gmail.com>
2022-05-04 22:39:14 +0300

    Create source.py

---
+++ src/source.py
@@ -0,0 +1,116 @@
+NOTICES = [
+    {
+        "Scottland": "https://resources.hse.gov.uk/notices/notices/notice_list.asp?PN=PAGENUMBER&rdoNType=&NT=&SN=P&SV=9&EO=%3D&SF=CTR&ST=N&SO=DNIS"
+    },
+    {
+        "England": "https://resources.hse.gov.uk/notices/notices/notice_list.asp?PN=PAGENUMBER&rdoNType=&NT=&SN=P&SV=8&EO=%3D&SF=CTR&ST=N&SO=DNIS"
+    },
+    {
+        "Wales": "https://resources.hse.gov.uk/notices/notices/notice_list.asp?PN=PAGENUMBER&rdoNType=&NT=&SN=P&SV=10&EO=%3D&SF=CTR&ST=N&SO=DNIS"
+    }
+]
+
+
+
+
+CASES = [
+    {
+        "ESE": "https://resources.hse.gov.uk/convictions/case/case_list.asp?PN=PAGENUMBER&ST=C&EO=%3D&SN=P&SF=UKR&SV=2&SO=ADN",
+    },
+    {
+        "London": "https://resources.hse.gov.uk/convictions/case/case_list.asp?PN=PAGENUMBER&ST=C&EO=%3D&SN=P&SF=UKR&SV=6&SO=ADN",
+    },
+    {
+        "Midlands": "https://resources.hse.gov.uk/convictions/case/case_list.asp?PN=PAGENUMBER&ST=C&EO=%3D&SN=P&SF=UKR&SV=5&SO=ADN",
+    },
+    {
+        "NW": "https://resources.hse.gov.uk/convictions/case/case_list.asp?PN=PAGENUMBER&ST=C&EO=%3D&SN=P&SF=UKR&SV=3&SO=ADN",
+    },
+    {
+        "Scotland": "https://resources.hse.gov.uk/convictions/case/case_list.asp?PN=PAGENUMBER&ST=C&EO=%3D&SN=P&SF=UKR&SV=7&SO=ADN",
+    },
+    {
+        "WalesSW": "https://resources.hse.gov.uk/convictions/case/case_list.asp?PN=PAGENUMBER&ST=C&EO=%3D&SN=P&SF=UKR&SV=1&SO=ADN",
+    },
+    {
+        "YorkNE": "https://resources.hse.gov.uk/convictions/case/case_list.asp?PN=PAGENUMBER&ST=C&EO=%3D&SN=P&SF=UKR&SV=4&SO=ADN"
+    }
+]
+
+
+
+CASES_HISTORY = [
+    {
+        "ESE": "https://resources.hse.gov.uk/convictions-history/case/case_list.asp?PN=PAGENUMBER&ST=C&EO=%3D&SN=P&SF=UKR&SV=2&SO=ADN",
+    },
+    {
+        "London": "https://resources.hse.gov.uk/convictions-history/case/case_list.asp?PN=PAGENUMBER&ST=C&EO=%3D&SN=P&SF=UKR&SV=6&SO=ADN",
+    },
+    {
+        "Midlands": "https://resources.hse.gov.uk/convictions-history/case/case_list.asp?PN=PAGENUMBER&ST=C&EO=%3D&SN=P&SF=UKR&SV=5&SO=ADN",
+    },
+    {
+        "NW": "https://resources.hse.gov.uk/convictions-history/case/case_list.asp?PN=PAGENUMBER&ST=C&EO=%3D&SN=P&SF=UKR&SV=3&SO=ADN",
+    },
+    {
+        "Scotland": "https://resources.hse.gov.uk/convictions-history/case/case_list.asp?PN=PAGENUMBER&ST=C&EO=%3D&SN=P&SF=UKR&SV=7&SO=ADN",
+    },
+    {
+        "WalesSW": "https://resources.hse.gov.uk/convictions-history/case/case_list.asp?PN=PAGENUMBER&ST=C&EO=%3D&SN=P&SF=UKR&SV=1&SO=ADN",
+    },
+    {
+        "YorkNE": "https://resources.hse.gov.uk/convictions-history/case/case_list.asp?PN=PAGENUMBER&ST=C&EO=%3D&SN=P&SF=UKR&SV=4&SO=ADN"
+    }
+]
+
+
+
+BREACHES = [
+    {
+        "ESE": "https://resources.hse.gov.uk/convictions/breach/breach_list.asp?PN=PAGENUMBER&ST=B&EO=%3D&SN=P&SF=UKR&SV=2&SO=DHD",
+    },
+    {
+        "London": "https://resources.hse.gov.uk/convictions/breach/breach_list.asp?PN=PAGENUMBER&ST=B&EO=%3D&SN=P&SF=UKR&SV=6&SO=DHD",
+    },
+    {
+        "Midlands": "https://resources.hse.gov.uk/convictions/breach/breach_list.asp?PN=PAGENUMBER&ST=B&EO=%3D&SN=P&SF=UKR&SV=5&SO=DHD",
+    },
+    {
+        "NW": "https://resources.hse.gov.uk/convictions/breach/breach_list.asp?PN=PAGENUMBER&ST=B&EO=%3D&SN=P&SF=UKR&SV=3&SO=DHD",
+    },
+    {
+        "Scotland": "https://resources.hse.gov.uk/convictions/breach/breach_list.asp?PN=PAGENUMBER&ST=B&EO=%3D&SN=P&SF=UKR&SV=7&SO=DHD",
+    },
+    {
+        "WalesSW": "https://resources.hse.gov.uk/convictions/breach/breach_list.asp?PN=PAGENUMBER&ST=B&EO=%3D&SN=P&SF=UKR&SV=1&SO=DHD",
+    },
+    {
+        "YorkNE": "https://resources.hse.gov.uk/convictions/breach/breach_list.asp?PN=PAGENUMBER&ST=B&EO=%3D&SN=P&SF=UKR&SV=4&SO=DHD"
+    }
+]
+
+
+
+BREACHES_HISTORY = [
+    {
+        "ESE": "https://resources.hse.gov.uk/convictions-history/breach/breach_list.asp?PN=PAGENUMBER&ST=B&EO=%3D&SN=P&SF=UKR&SV=2&SO=DHD",
+    },
+    {
+        "London": "https://resources.hse.gov.uk/convictions-history/breach/breach_list.asp?PN=PAGENUMBER&ST=B&EO=%3D&SN=P&SF=UKR&SV=6&SO=DHD",
+    },
+    {
+        "Midlands": "https://resources.hse.gov.uk/convictions-history/breach/breach_list.asp?PN=PAGENUMBER&ST=B&EO=%3D&SN=P&SF=UKR&SV=5&SO=DHD",
+    },
+    {
+        "NW": "https://resources.hse.gov.uk/convictions-history/breach/breach_list.asp?PN=PAGENUMBER&ST=B&EO=%3D&SN=P&SF=UKR&SV=3&SO=DHD",
+    },
+    {
+        "Scotland": "https://resources.hse.gov.uk/convictions-history/breach/breach_list.asp?PN=PAGENUMBER&ST=B&EO=%3D&SN=P&SF=UKR&SV=7&SO=DHD",
+    },
+    {
+        "WalesSW": "https://resources.hse.gov.uk/convictions-history/breach/breach_list.asp?PN=PAGENUMBER&ST=B&EO=%3D&SN=P&SF=UKR&SV=1&SO=DHD",
+    },
+    {
+        "YorkNE": "https://resources.hse.gov.uk/convictions-history/breach/breach_list.asp?PN=PAGENUMBER&ST=B&EO=%3D&SN=P&SF=UKR&SV=4&SO=DHD"
+    }
+]
